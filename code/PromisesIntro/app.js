95dcabda8854ace40f7e32056b4e63de1b4016c5
Christopher Chua <chuachristopher@hotmail.com>
2020-05-20 12:54:00 +0800

    Promise Chaining

--- code/PromisesIntro/app.js
+++ code/PromisesIntro/app.js
@@ -1,3 +1,4 @@
+/*
 // PROMISES Intro
 const willGetYouADog = new Promise((resolve, reject) => {
 	const rand = Math.random();
@@ -14,7 +15,7 @@ willGetYouADog.then(() => {
 willGetYouADog.catch(() => {
 	console.log(':( nooo');
 })
-
+//
 // Returning Promises from Functions
 const makeDogPromise = () => {
 	return new Promise((resolve, reject) => {
@@ -35,4 +36,77 @@ makeDogPromise()
 	})
 	.catch(() => {
 		console.log(':( NO DOG');
+	});
+*/
+//
+// Resolving/Rejecting with Values
+const fakeRequest = (url) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const pages = {
+				'/users' : [
+					{id: 1, username: 'Bilbo'},
+					{id: 2, username: 'Esmerelda'}
+				],
+				'/users/1'      : {
+					id        : 1,
+					username  : 'Bilbo',
+					upvotes   : 360,
+					city      : 'Lisbon',
+					topPostId : 454321
+				},
+				'/users/5'      : {
+					id       : 5,
+					username : 'Esmerelda',
+					upvotes  : 571,
+					city     : 'Honolulu'
+				},
+				'/posts/454321' : {
+					id    : 454321,
+					title :
+						'Ladies & Gentlemen, may I introduce my pet pig, Hamlet'
+				},
+				'/about' : 'This is the about page!'
+			}
+			const data = pages[url];
+			if (data) {
+				resolve({status: 200, data});
+			}
+			else {
+				reject({status: 404});
+			}
+		}, 1000);
+	})
+}
+
+// fakeRequest('/about')
+// 	.then((response) => {
+// 		console.log(response.status);
+// 		console.log(response.data);
+// 		console.log("Request OK");
+// 	})
+// 	.catch((response) => {
+// 		console.log(response.status);
+// 		console.log('Request failed');
+// 	})
+
+// Clean Promise Chaining
+// instead of nesting our .then() s, we return the promise object after every step.
+// we only need ONE .catch()!
+fakeRequest('/users')
+	.then((res) => {
+		console.log(res);
+		const id = res.data[0].id;
+		return fakeRequest(`/users/${id}`);
+	})
+	.then((res) => {
+		console.log(res);
+		const postId = res.data.topPostId;
+		return fakeRequest(`/posts/${postId}`);
+	})
+	.then((res) => {
+		console.log(res);
+	})
+	.catch((err) => {
+		console.log('OH NO!', err);
 	});
