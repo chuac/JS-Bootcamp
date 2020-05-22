5e7ab7eea3d39717e67338ca3b027102157cb466
Christopher Chua <chuachristopher@hotmail.com>
2020-05-22 14:01:43 +0800

    Refactored Fetch chains

--- code/HTTPRequests/app.js
+++ code/HTTPRequests/app.js
@@ -69,28 +69,60 @@
 
 //// Chaining Fetch requests, cleaner with Promises!
 
+// fetch('https://swapi.dev/api/planets/')
+// 	.then((response) => {
+// 		if (!response.ok) {
+// 			throw new Error(`Status Code Error: ${response.status}`); // throwing error will trigger our catch
+// 		}
+// 		return response.json();
+// 	})
+// 	.then((data) => {
+// 		console.log('Fetched planets data');
+// 		const filmURL = data.results[0].films[0];
+// 		return fetch(filmURL);
+// 	})
+// 	.then((response) => {
+// 		if (!response.ok) {
+// 			throw new Error(`Status Code Error: ${response.status}`); // throwing error will trigger our catch
+// 		}
+// 		return response.json();
+// 	})
+// 	.then((data) => {
+// 		console.log('Fetched film data');
+// 		console.log(data.title);
+// 	})
+// 	.catch((err) => { // but doesn't catch when we receive 404 error! only when internet not working
+// 		console.log('Something went wrong with Fetch');
+// 		console.log(err);
+// 	})
+
+
+//// Refactoring Fetch chains
+
+const checkStatusAndParse = (response) => {
+	if (!response.ok) {
+		throw new Error(`Status Code Error: ${response.status}`); // throwing error will trigger our catch
+	}
+	return response.json();
+}
+
+const printPlanets = (data) => {
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	return Promise.resolve(data.next); // need to return a Promise object to allow chaining to continue
+}
+
+const fetchNextPlanets = (url) => {
+	return fetch(url);
+}
+
 fetch('https://swapi.dev/api/planets/')
-	.then((response) => {
-		if (!response.ok) {
-			throw new Error(`Status Code Error: ${response.status}`); // throwing error will trigger our catch
-		}
-		return response.json();
-	})
-	.then((data) => {
-		console.log('Fetched planets data');
-		const filmURL = data.results[0].films[0];
-		return fetch(filmURL);
-	})
-	.then((response) => {
-		if (!response.ok) {
-			throw new Error(`Status Code Error: ${response.status}`); // throwing error will trigger our catch
-		}
-		return response.json();
-	})
-	.then((data) => {
-		console.log('Fetched film data');
-		console.log(data.title);
-	})
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(checkStatusAndParse)
+	.then(printPlanets)
 	.catch((err) => { // but doesn't catch when we receive 404 error! only when internet not working
 		console.log('Something went wrong with Fetch');
 		console.log(err);
