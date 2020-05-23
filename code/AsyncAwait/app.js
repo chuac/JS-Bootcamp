1c767ce2d6854c5951ff35e945d3f94ef5dbb2c1
Christopher Chua <chuachristopher@hotmail.com>
2020-05-23 10:02:11 +0800

    Improving Callback Hell code using AsyncAwait

--- code/AsyncAwait/app.js
+++ code/AsyncAwait/app.js
@@ -18,13 +18,51 @@
 
 //// now with error handling 
 
-async function getPlanets() {
-    try {
-        const res = await axios.get('https://swapi.dev/api/planets/');
-        console.log(res.data); // only runs once the previous line is complete
-    } catch (e) {
-        console.log('In CATCH!', e);
-    }
+// async function getPlanets() {
+//     try {
+//         const res = await axios.get('https://swapi.dev/api/planets/');
+//         console.log(res.data); // only runs once the previous line is complete
+//     } catch (e) {
+//         console.log('In CATCH!', e);
+//     }
+// }
+
+// getPlanets();
+
+////------------------------------------------------------------------------------
+//// Improving Callback Hell code using AsyncAwait
+
+const moveX = (element, amount, delay) => { // instead of just one callback available, we now have two callback paths: onSuccess, onFailure
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const bodyBoundary = document.body.clientWidth;
+			const elRight = element.getBoundingClientRect().right;
+			const currLeft = element.getBoundingClientRect().left; // the el's current pos so we can just add this to the translate amount requested
+			if (elRight + amount > bodyBoundary) { // user is wanting to move outside the body width
+				reject({bodyBoundary, elRight, amount});
+			}
+			else {
+				element.style.transform = `translateX(${currLeft + amount}px)`;
+				resolve();
+			}
+		}, delay);
+	});
+};
+
+const btn = document.querySelector('button');
+
+async function animateRight(el) {
+    await moveX(el, 100, 1000);
+    await moveX(el, 100, 1000);
+    await moveX(el, 100, 1000);
+    await moveX(el, 100, 1000);
+    await moveX(el, 100, 1000);
+    await moveX(el, 100, 1000);
+    await moveX(el, 100, 1000);
+    await moveX(el, 100, 1000);
 }
 
-getPlanets();
+animateRight(btn)
+    .catch((err) => {
+        console.log('All Done!');
+    });
