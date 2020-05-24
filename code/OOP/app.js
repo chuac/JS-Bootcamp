b7a78dd04ef9f2c1f17df995b9159660178ca66c
Christopher Chua <chuachristopher@hotmail.com>
2020-05-24 19:04:58 +0800

    OOP more practice with Classes

--- code/OOP/app.js
+++ code/OOP/app.js
@@ -58,12 +58,43 @@
 
 //// Class keyword
 
+// class Colour {
+// 	constructor(r, g, b, name) { // need to have this constructor named constructor
+// 		this.r = r;
+// 		this.g = g;
+// 		this.b = b;
+// 		this.name = name;
+// 	}
+// 	innerRGB() {
+// 		const {r, g, b} = this;
+//  		return `${r}, ${g}, ${b}`;
+// 	}
+// 	rgb() {
+//  		return `rgb(${this.innerRGB()})`; // showcasing calling a method from another method within a class
+// 	}
+// 	rgba(alpha = 1.0) {
+// 		return `rgba(${this.innerRGB()}, ${alpha})`;
+// 	}
+// 	hex() {
+// 		const {r, g, b} = this;
+// 		return (
+// 			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+// 		);
+// 	}
+// }
+
+// const c1 = new Colour(255, 67, 89, 'tomato');
+
+
+//// More practice with Classes. Code to make HSL colours were provided
+
 class Colour {
 	constructor(r, g, b, name) { // need to have this constructor named constructor
 		this.r = r;
 		this.g = g;
 		this.b = b;
 		this.name = name;
+		this.calcHSL(); // we can call a method straight away in the constructor!
 	}
 	innerRGB() {
 		const {r, g, b} = this;
@@ -81,6 +112,64 @@ class Colour {
 			'#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
 		);
 	}
+	hsl() {
+		const {h, s, l} = this;
+		return `hsl(${h}, ${s}%, ${l}%)`;
+	}
+	fullySaturated() {
+		const {h, l} = this;
+		return `hsl(${h}, 100%, ${l}%)`;
+	}
+	opposite() {
+		const {h, s, l} = this;
+		const newHue = (h + 180) % 360;
+		return `hsl(${newHue}, ${s}%, ${l}%)`;
+	}
+	calcHSL() {
+		let { r, g, b } = this;
+		// Make r, g, and b fractions of 1
+		r /= 255;
+		g /= 255;
+		b /= 255;
+	
+		// Find greatest and smallest channel values
+		let cmin = Math.min(r, g, b),
+			cmax = Math.max(r, g, b),
+			delta = cmax - cmin,
+			h = 0,
+			s = 0,
+			l = 0;
+		if (delta == 0) h = 0;
+		else if (cmax == r)
+			// Red is max
+			h = ((g - b) / delta) % 6;
+		else if (cmax == g)
+			// Green is max
+			h = (b - r) / delta + 2;
+		else
+			// Blue is max
+			h = (r - g) / delta + 4;
+	
+		h = Math.round(h * 60);
+	
+		// Make negative hues positive behind 360°
+		if (h < 0) h += 360;
+		// Calculate lightness
+		l = (cmax + cmin) / 2;
+	
+		// Calculate saturation
+		s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+	
+		// Multiply l and s by 100
+		s = +(s * 100).toFixed(1);
+		l = +(l * 100).toFixed(1);
+
+		this.h = h;
+		this.s = s;
+		this.l = l;
+	}
 }
 
-const c1 = new Colour(255, 67, 89, 'tomato');
+let tomato = new Colour(255, 67, 89, 'tomato');
+let white = new Colour(255, 255, 255, 'white');
+
