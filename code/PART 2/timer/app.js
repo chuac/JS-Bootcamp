6531a8e83605efaaa2836787497d256d01ab12bd
Christopher Chua <chuachristopher@hotmail.com>
2020-05-25 10:35:52 +0800

    Timer app passing in callback functions

--- code/PART 2/timer/app.js
+++ code/PART 2/timer/app.js
@@ -1,42 +1,18 @@
-//// DOM-centric approach
-
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-
-        this.startButton.addEventListener('click', this.start); // listen for a click event, and when it occurs, call this.start method
-        this.pauseButton.addEventListener('click', this.pause);
-    }
-
-    start = () => { // binded to the event listener declared in the constructor. as an arrow function to solve our 'this' issue
-        this.tick();
-        this.intervalID = setInterval(this.tick, 1000); // timer ID is saved to be used in pause()
-    }
-    pause = () => {
-        clearInterval(this.intervalID);
-    }
-    tick = () => {
-        if (this.timeRemaining <= 0) {
-            this.pause();
-        }
-        else {
-            this.timeRemaining = this.timeRemaining - 1; // setter on the left, getter on the right - 1.
-        }
-        
-    }
-    get timeRemaining() { // getter
-        return parseFloat(this.durationInput.value); // html values are strings so we need to convert
-    }
-    set timeRemaining(time) { // setter
-        this.durationInput.value = time;
-    }
-}
+//// DOM-centric approach. Timer Class is in timer.js
 
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+        console.log("timer started");
+    },
+    onTick() {
+        console.log("timer ticking");
+    },
+    onComplete() {
+        console.log("timer completed");
+    }
+}); // passing in 3 callback functions
 
---
+++ code/PART 2/timer/timer.js
@@ -0,0 +1,50 @@
+//// DOM-centric approach
+
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        if (callbacks) { // passing in callback functions are optional
+            this.onStart = callbacks.onStart; // onStart key in this object = onStart key (function) in the callbacks object passed in
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+
+        this.startButton.addEventListener('click', this.start); // listen for a click event, and when it occurs, call this.start method
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => { // binded to the event listener declared in the constructor. as an arrow function to solve our 'this' issue
+        if (this.onStart) { // if a callback was passed in
+            this.onStart();
+        }
+        this.tick();
+        this.intervalID = setInterval(this.tick, 1000); // timer ID is saved to be used in pause()
+    }
+    pause = () => {
+        clearInterval(this.intervalID);
+    }
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            if (this.onComplete) { // if a callback was passed in
+                this.onComplete();
+            }
+            this.pause();
+        }
+        else {
+            if (this.onTick) { // if a callback was passed in
+                this.onTick();
+            }
+            this.timeRemaining = this.timeRemaining - 1; // setter on the left, getter on the right - 1.
+        }
+        
+    }
+    get timeRemaining() { // getter
+        return parseFloat(this.durationInput.value); // html values are strings so we need to convert
+    }
+    set timeRemaining(time) { // setter
+        this.durationInput.value = time;
+    }
+}
