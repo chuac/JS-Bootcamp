32db2b7343646c0907576087c3551b8b407742f9
Christopher Chua <chuachristopher@hotmail.com>
2020-05-27 11:19:30 +0800

    Movie Fight app first step refactoring

---
+++ code/PART 2/movie-fight/autocomplete.js
@@ -0,0 +1,52 @@
+const createAutoComplete = ({root}) => {
+    // we now accept a root element
+    root.innerHTML = `
+        <label><b>Search For a Movie</b></label>
+        <input class="input" />
+        <div class="dropdown">
+            <div class="dropdown-menu">
+                <div class="dropdown-content results"></div>
+            </div>
+        </div>
+    `;
+
+    const input = root.querySelector('input'); // no longer searching in document, but the root element passed in and generated above
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWrapper = root.querySelector('.results');
+
+    const onInput = async (event) => { // could wrap debounce down below too
+        const movies = await fetchData(event.target.value);
+        
+        if (!movies.length) { // if empty movies result, close the dropdown menu and exit function
+            dropdown.classList.remove('is-active');
+            return;
+        }
+
+        resultsWrapper.innerHTML = ''; // empty results before each new call
+        dropdown.classList.add('is-active'); // open the dropdown menu. Bulma CSS class
+        for (let movie of movies) {
+            const option = document.createElement('a'); // an option for the user to click on
+            const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster; // if Poster is 'N/A' from the API
+            option.classList.add('dropdown-item'); // Bulma CSS class
+            option.innerHTML = `
+                <img src="${imgSRC}"/>
+                ${movie.Title}
+            `;
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active'); // user clicked on an option so we close the dropdown menu
+                input.value = movie.Title; // update the input box with the user's selection
+                onMovieSelect(movie);
+            })
+            
+            resultsWrapper.appendChild(option);
+        }
+    };
+
+    input.addEventListener('input', debounce(onInput, 1000)); // we could wrap debounce around this calling of onInput too
+
+    document.addEventListener('click', (event) => { // listen for a click anywhere in the document
+        if (!root.contains(event.target)) { // if the click target doesn't contain our 'root' element which contains our dropdown menu stuff
+            dropdown.classList.remove('is-active'); // close the dropdown menu! Bulma CSS class
+        }
+    });
+};
--- code/PART 2/movie-fight/index.js
+++ code/PART 2/movie-fight/index.js
@@ -14,57 +14,16 @@ const fetchData = async (searchTerm) => {
     return response.data.Search; // the API's key is using capital S
 }
 
-//// Moving creating HTML for our widget inside our JS files to de-couple from our HTML file. Helps with re-usability
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-    <label><b>Search For a Movie</b></label>
-    <input class="input" />
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
-    </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (event) => { // could wrap debounce down below too
-    const movies = await fetchData(event.target.value);
-    
-    if (!movies.length) { // if empty movies result, close the dropdown menu and exit function
-        dropdown.classList.remove('is-active');
-        return;
-    }
-
-    resultsWrapper.innerHTML = ''; // empty results before each new call
-    dropdown.classList.add('is-active'); // open the dropdown menu. Bulma CSS class
-    for (let movie of movies) {
-        const option = document.createElement('a'); // an option for the user to click on
-        const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster; // if Poster is 'N/A' from the API
-        option.classList.add('dropdown-item'); // Bulma CSS class
-        option.innerHTML = `
-            <img src="${imgSRC}"/>
-            ${movie.Title}
-        `;
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-active'); // user clicked on an option so we close the dropdown menu
-            input.value = movie.Title; // update the input box with the user's selection
-            onMovieSelect(movie);
-        })
-        
-        resultsWrapper.appendChild(option);
-    }
-};
-
-input.addEventListener('input', debounce(onInput, 1000)); // we could wrap debounce around this calling of onInput too
-
-document.addEventListener('click', (event) => { // listen for a click anywhere in the document
-    if (!root.contains(event.target)) { // if the click target doesn't contain our 'root' element which contains our dropdown menu stuff
-        dropdown.classList.remove('is-active'); // close the dropdown menu! Bulma CSS class
-    }
+createAutoComplete({ // give it one root element to render our autocomplete into
+    root: document.querySelector('.autocomplete')
 });
+createAutoComplete({
+    root: document.querySelector('.autocomplete-two')
+});
+createAutoComplete({
+    root: document.querySelector('.autocomplete-three')
+});
+
 
 const onMovieSelect = async (movie) => {
     const response = await axios.get('http://www.omdbapi.com/', {
@@ -112,5 +71,5 @@ const movieTemplate = (movieDetail) => { // movieDetail is the more in-depth mov
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
-    `;
+    `; // Bulma CSS classes
 }
