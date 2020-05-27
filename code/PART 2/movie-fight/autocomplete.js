203e4f634ef98d2454c68c39f7bd56862a7d4a97
Christopher Chua <chuachristopher@hotmail.com>
2020-05-27 11:45:21 +0800

    Movie Fight app refactored fetchData() and references to movies

--- code/PART 2/movie-fight/autocomplete.js
+++ code/PART 2/movie-fight/autocomplete.js
@@ -1,7 +1,7 @@
-const createAutoComplete = ({root, renderOption, onOptionSelect, inputValueWhenClicked}) => {
-    // we now accept a root element
+const createAutoComplete = ({root, renderOption, onOptionSelect, inputValueWhenClicked, fetchData}) => {
+    // we now accept a root element to render stuff into
     root.innerHTML = `
-        <label><b>Search For a Movie</b></label>
+        <label><b>Search</b></label>
         <input class="input" />
         <div class="dropdown">
             <div class="dropdown-menu">
@@ -14,32 +14,32 @@ const createAutoComplete = ({root, renderOption, onOptionSelect, inputValueWhenC
     const dropdown = root.querySelector('.dropdown');
     const resultsWrapper = root.querySelector('.results');
 
-    const onInput = async (event) => { // could wrap debounce down below too
-        const movies = await fetchData(event.target.value);
+    const onInput = async (event) => { // could wrap debounce here too
+        const items = await fetchData(event.target.value);
         
-        if (!movies.length) { // if empty movies result, close the dropdown menu and exit function
+        if (!items.length) { // if empty items result, close the dropdown menu and exit function
             dropdown.classList.remove('is-active');
             return;
         }
 
         resultsWrapper.innerHTML = ''; // empty results before each new call
         dropdown.classList.add('is-active'); // open the dropdown menu. Bulma CSS class
-        for (let movie of movies) {
+        for (let item of items) {
             const option = document.createElement('a'); // an option for the user to click on
             
             option.classList.add('dropdown-item'); // Bulma CSS class
-            option.innerHTML = renderOption(movie);
+            option.innerHTML = renderOption(item);
             option.addEventListener('click', () => {
                 dropdown.classList.remove('is-active'); // user clicked on an option so we close the dropdown menu
-                input.value = inputValueWhenClicked(movie); // update the input box with the user's selection
-                onOptionSelect(movie);
+                input.value = inputValueWhenClicked(item); // update the input box with the user's selection
+                onOptionSelect(item);
             })
             
             resultsWrapper.appendChild(option);
         }
     };
 
-    input.addEventListener('input', debounce(onInput, 1000)); // we could wrap debounce around this calling of onInput too
+    input.addEventListener('input', debounce(onInput, 1000)); // we could wrap debounce around calling of onInput too
 
     document.addEventListener('click', (event) => { // listen for a click anywhere in the document
         if (!root.contains(event.target)) { // if the click target doesn't contain our 'root' element which contains our dropdown menu stuff
--- code/PART 2/movie-fight/index.js
+++ code/PART 2/movie-fight/index.js
@@ -1,21 +1,6 @@
-
-const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: { // param keys all lowercase
-            apikey: 'e7a59c4a',
-            s: searchTerm
-        }
-    });
-
-    if (response.data.Error) { // we didn't get any results
-        return []; // return an empty array so our iteration down below doesn't break
-    }
-
-    return response.data.Search; // the API's key is using capital S
-}
-
-createAutoComplete({ // give it one root element to render our autocomplete into
-    root: document.querySelector('.autocomplete'),
+// pass a "config" object that contains all we need to customise our autocomplete for what we want. in this case: movies
+createAutoComplete({ 
+    root: document.querySelector('.autocomplete'), // give it one root element to render our autocomplete into
     renderOption(movie) { // how we want our options in the dropdown menu to look like
         const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster; // if Poster is 'N/A' from the API
         return `
@@ -28,6 +13,18 @@ createAutoComplete({ // give it one root element to render our autocomplete into
     },
     inputValueWhenClicked(movie) { // tell the widget what value to insert into input box's value when an option is selected
         return movie.Title;
+    },
+    async fetchData(searchTerm) {
+        const response = await axios.get('http://www.omdbapi.com/', {
+            params: { // param keys all lowercase
+                apikey: 'e7a59c4a',
+                s: searchTerm
+            }
+        });
+        if (response.data.Error) { // we didn't get any results
+            return []; // return an empty array so our iteration down below doesn't break
+        }
+        return response.data.Search; // the API's key is using capital S
     }
 });
 
