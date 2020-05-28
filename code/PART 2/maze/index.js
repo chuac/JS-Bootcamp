798dc91bcdaa2fe4c725be6addb4a4117070a197
Christopher Chua <chuachristopher@hotmail.com>
2020-05-28 17:47:05 +0800

    Canvas API Matter-js intro added walls and random shapes

--- code/PART 2/maze/index.js
+++ code/PART 2/maze/index.js
@@ -1,6 +1,9 @@
 //console.log('hi');
 
-const { Engine, Render, Runner, World, Bodies } = Matter; // Matter is imported from cdnjs
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter; // Matter is imported from cdnjs
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine; // when you create an engine, you get a world along with it
@@ -8,14 +11,38 @@ const render = Render.create({
     element: document.body, // "go and render the representation of World inside document.body"
     engine: engine, // specify which engine to use
     options: {
-        width: 800,
-        height: 600
+        wireframes: false, // setting to false will show solid shapes
+        width: width,
+        height: height
     }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 100, 200, { // create a shape/Body. centerpoint at 200, 200. Width 100, height 200
-    isStatic: true // we want the body to never move
-});
-World.add(world, shape); // add the shape to our world object
+// adding ability to click-and-drag shapes
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}));
+
+// Walls
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, {isStatic: true}), // top wall
+    Bodies.rectangle(400, 600, 800, 40, {isStatic: true}), // bottom wall
+    Bodies.rectangle(0, 300, 40, 600, {isStatic: true}), // left wall
+    Bodies.rectangle(800, 300, 40, 600, {isStatic: true}), // right wall
+];
+World.add(world, walls); // can also pass in an array of shapes/bodies
+
+// Random Shapes starting at random positions
+for (let i = 0; i < 20; i++) {
+    if (Math.random() > 0.5) { // randomiser
+        World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, {}));
+    } else {
+        World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+            render: {
+                fillStyle: 'green' // all circles will be green now
+            }
+        }));
+    }
+    
+}
