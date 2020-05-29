7bb38115615d7d05647d5952061af6fb75d48993
Christopher Chua <chuachristopher@hotmail.com>
2020-05-29 22:16:55 +0800

    Node ls-clone Option #2 wrap lstat with Promise, but only running in serial (not parallel)

--- code/PART 2/ls-clone/index.js
+++ code/PART 2/ls-clone/index.js
@@ -5,8 +5,31 @@
 const fs = require('fs'); // File System module in Node is abbreviated as fs
 // the Process module is automatically included in every file
 
+
+//// Method #1. Wrapping lstat inside a Promise
+// const lstat = (filename) => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//             if (err) {
+//                 reject(err);
+//             }
+
+//             resolve(stats);
+//         })
+//     })
+// }
+
+//// Method #2, util.promisify
+// const util = require('util');
+
+// const lstat = util.promisify(fs.lstat);
+
+//// Method #3, using built in Promises API with fs
+//// https://nodejs.org/api/fs.html#fs_fspromises_lstat_path_options
+const { lstat } = fs.promises; 
+
 //// https://nodejs.org/api/fs.html#fs_fs_readdir_path_options_callback
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
     // EITHER
     // err === an error object, which means something went wrong
     // OR
@@ -16,26 +39,15 @@ fs.readdir(process.cwd(), (err, filenames) => {
         console.log(err);
     }
 
-    const allStats = Array(filenames.length).fill(null);
-
     for (let filename of filenames) {
-        const index = filenames.indexOf(filename); // get loop index
+        try {
+            const stats = await lstat(filename);
 
-        //// https://nodejs.org/api/fs.html#fs_fs_lstat_path_options_callback
-        fs.lstat(filename, (err, stats) => {
-            if (err) {
-                console.log(err);
-            }
-            allStats[index] = stats; // contain all the returned stats objects, dont print them first
-            const ready = allStats.every((stats) => { // will only return true if EVERY element in allStats is not a falsy value (not null)
-                return stats; // null or our actual object
-            });
-
-            if (ready) { // we have captured all the returned objects, now we print them out in correct order
-                allStats.forEach((stats, index) => {
-                    console.log(filenames[index], stats.isFile());
-                })
-            }
-        });
+            console.log(filename, stats.isFile());
+        } catch (err) {
+            console.log(err);
+        }
     }
+ 
 });
+
