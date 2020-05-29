591ec81a505288e2331f4f0acd61d1357c56ce0e
Christopher Chua <chuachristopher@hotmail.com>
2020-05-29 18:34:33 +0800

    Node the Require Cache, and demo of how files only get required once

--- code/PART 2/node/index.js
+++ code/PART 2/node/index.js
@@ -1,3 +1,9 @@
-const messages = require('./myscript.js');
+const counterObject = require('./myscript.js');
 
-console.log(messages);
+console.log(counterObject.getCounter()); // should return 0
+counterObject.incrementCounter();
+console.log(counterObject.getCounter()); // should return 1
+
+const newCounterObject = require('./myscript.js');
+
+console.log(newCounterObject.getCounter()); // do we expect 0? nope, it returns 1 because of the require cache
