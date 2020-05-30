298f96fa84e247e5b319e3c9a635e8e1633085d0
Christopher Chua <chuachristopher@hotmail.com>
2020-05-30 15:18:43 +0800

    Ecommerce displaying basic HTML

--- code/PART 2/ecommerce/index.js
+++ code/PART 2/ecommerce/index.js
@@ -3,7 +3,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { // request, response...at the root route
-    res.send('hi there');
+    res.send(`
+        <div>
+            <form>
+                <input placeholder="email" />
+                <input placeholder="password" />
+                <input placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
 });
 
 app.listen(3000, () => { // access at 'localhost:3000'
