01eb0e247c17dc56a0bf99d378aaf135531c30a5
Christopher Chua <chuachristopher@hotmail.com>
2020-05-30 12:13:58 +0800

    Node Watch-it added Caporal, handling CLI arguments and checking if file exists

--- code/PART 2/watch-it/index.js
+++ code/PART 2/watch-it/index.js
@@ -1,9 +1,31 @@
 #!/usr/bin/env node
 
+//// https://nodejs.org/api/fs.html
+const fs = require('fs');
 //// https://www.npmjs.com/package/chokidar
 const chokidar = require('chokidar');
 //// https://www.npmjs.com/package/lodash.debounce
 const debounce = require('lodash.debounce');
+//// https://www.npmjs.com/package/caporal
+const program = require('caporal'); // the use of name 'program' is recommended in the Caporal docs
+
+
+program
+    .version('0.0.1')
+    .argument('[filename]', 'Name of a file to execute')
+    .action(async ({ filename }) => { // destructure filename out of args
+        const name = filename || 'index.js'; // default to index.js if no filename provided
+        
+        try {
+            await fs.promises.access(name); // check if file exists
+        } catch (err) {
+            throw new Error(`Could not find the file ${name}`);
+        }
+        
+        console.log(args);
+    });
+
+program.parse(process.argv);
 
 
 const start = debounce(() => {
@@ -12,5 +34,5 @@ const start = debounce(() => {
 
 chokidar.watch('.')
     .on('add', start)
-    .on('change', () => { console.log('file changed')})
-    .on('unlink', () => { console.log('file unliked')})
+    .on('change', start)
+    .on('unlink', start)
