9f537ef4c834a16dfd6d94af19c23f9df17cd84d
Christopher Chua <chuachristopher@hotmail.com>
2020-05-30 16:07:11 +0800

    Ecommerce our basic Middleware parser

--- code/PART 2/ecommerce/index.js
+++ code/PART 2/ecommerce/index.js
@@ -15,19 +15,30 @@ app.get('/', (req, res) => { // request, response...at the root route
     `);
 });
 
-app.post('/', (req, res) => {
-    req.on('data', (data) => { // 'on' can be thought of as an addEventListener listening on the 'data' event
+const bodyParser = (req, res, next) => {
+    if (req.method === 'POST') { // the method property inside the req object
+        req.on('data', (data) => { // 'on' can be thought of as an addEventListener listening on the 'data' event
         const parsed = data.toString('utf8').split('&');; // 'data' is just a data Buffer, need to convert it
         const formData = {};
         for (let pair of parsed) {
             const [key, value] = pair.split('='); // destructuring split output into key and value variables
             formData[key] = value;
         }
-        console.log(formData);
-    })
+        req.body = formData;
+        next(); // Express callback function given to us. Tell Express we are done here with our Middleware
+        });
+    } else {
+        next();
+    }
     
+}
+
+app.post('/', bodyParser, (req, res) => {
+    
+    console.log(req.body);
     res.send('Account created');
 });
+
 
 app.listen(3000, () => { // access at 'localhost:3000'
     console.log('Listening');
