2344391ca72c709e2c05383bf4c25856a962f29d
Christopher Chua <chuachristopher@hotmail.com>
2020-05-30 14:02:05 +0800

    Node Watch-it CLI app

--- code/PART 2/watch-it/index.js
+++ code/PART 2/watch-it/index.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
+//// in terminal, try running 'watchit test.js' or 'watchit -h' for help
+
 //// https://nodejs.org/api/fs.html
 const fs = require('fs');
+//// https://nodejs.org/api/child_process.html
+const { spawn } = require('child_process');
 //// https://www.npmjs.com/package/chokidar
 const chokidar = require('chokidar');
 //// https://www.npmjs.com/package/lodash.debounce
 const debounce = require('lodash.debounce');
 //// https://www.npmjs.com/package/caporal
 const program = require('caporal'); // the use of name 'program' is recommended in the Caporal docs
+//// https://www.npmjs.com/package/chalk
+const chalk = require('chalk');
 
 
 program
@@ -21,18 +27,19 @@ program
         } catch (err) {
             throw new Error(`Could not find the file ${name}`);
         }
-        
-        console.log(args);
+        let proc;
+        const start = debounce(() => {
+            if (proc) { // check if proc (spawn object) already exists, then kill it before starting a new spawn
+                proc.kill();
+            }
+            console.log(chalk.green.bold('>>>> Starting process...'));
+            proc = spawn('node', [name], { stdio: 'inherit' });
+        }, 500);
+
+        chokidar.watch('.')
+            .on('add', start)
+            .on('change', start)
+            .on('unlink', start)
     });
 
 program.parse(process.argv);
-
-
-const start = debounce(() => {
-    console.log('starting program');
-}, 100);
-
-chokidar.watch('.')
-    .on('add', start)
-    .on('change', start)
-    .on('unlink', start)
---
+++ code/PART 2/watch-it/test.js
@@ -0,0 +1,5 @@
+//// test file to run our watch-it program on. in terminal, try running 'watchit test.js' or 'watchit -h' for help
+
+setInterval(() => {
+    console.log('bye there')
+}, 1000);
