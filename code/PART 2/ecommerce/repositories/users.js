56940e38f94044e4ae0d9e9803f8a9b4d7af252d
Christopher Chua <chuachristopher@hotmail.com>
2020-05-31 13:49:59 +0800

    Ecommerce custom db finished UsersRepository methods

--- code/PART 2/ecommerce/repositories/users.js
+++ code/PART 2/ecommerce/repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
     constructor(filename) { // not allowed to have async code in here
@@ -21,24 +22,82 @@ class UsersRepository {
     }
 
     async create(attrs) { // attrs is an attributes object of the new user
+        attrs.id = this.randomId();
+
         const records = await this.getAll();
         records.push(attrs);
 
-        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+        await this.writeAll(records);
+    }
+
+    async writeAll(records) {
+        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2)); // stringify takes arguments to make the JSON file easier to read
+    }
+
+    randomId() { 
+        return crypto.randomBytes(4).toString('hex'); // https://nodejs.org/api/crypto.html#crypto_crypto_randombytes_size_callback
+    }
+
+    async getOne(id) {
+        const records = await this.getAll();
+
+        return records.find((record) => record.id === id);
+    }
+
+    async delete(id) {
+        const records = await this.getAll();
+
+        const filteredRecords = records.filter((record) => record.id !== id); // get a records data object that doesn't contain where record.id === id
+
+        await this.writeAll(filteredRecords); // write the data without this removed user back into the .json file
+    }
+
+    async update(id, attrs) { // attrs is an object 
+        const records = await this.getAll();
+        const record = records.find((record) => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`);
+        }
+
+        Object.assign(record, attrs); // takes all properties (key: value pairs) from attrs and copies them over into record
+
+        await this.writeAll(records);
+    }
+
+    async getOneBy(filters) { // filters is an object containing key: value pairs to match by
+        const records = await this.getAll();
+
+        for (let record of records) { // for-of loop iterating over array
+            let found = true; // flag
+            for (let key in filters) { // for-in loop iterating over object
+                if (record[key] !== filters[key]) { // if key:value pair at this record doesn't match the filter(s)
+                    found = false; // flip flag
+                }
+            }
+
+            if (found) { // if flag never flipped to false that means there was a match
+                return record;
+            }
+        }
     }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.create({
-        email: "test@test.com",
-        password: "password"
-    })
-    
-    const users = await repo.getAll();
+    // await repo.create({
+    //     email: "test@test.com",
+    //     password: "password"
+    // })
 
-    console.log(users);
+    
+    //const user = await repo.delete('570b6651');
+    const user = await repo.getOneBy({name: 'James'});
+    const users = await repo.getAll();
+    console.log(user);
+
+
 }
 test();
 
