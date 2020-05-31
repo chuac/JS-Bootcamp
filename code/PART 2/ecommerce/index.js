607eed27b8d91a11375eaa8680fb75c33ef3d03c
Christopher Chua <chuachristopher@hotmail.com>
2020-05-31 16:31:09 +0800

    Ecommerce added signout, signin routes

--- code/PART 2/ecommerce/index.js
+++ code/PART 2/ecommerce/index.js
@@ -11,7 +11,7 @@ app.use(cookieSession({
     keys: ['eagsgdsfjdgaifieaflayaaq']
 })); // cookie-session will encrypt the info we store in the cookie, when we provide a key string
 
-app.get('/', (req, res) => { // request, response...at the root route
+app.get('/signup', (req, res) => { // request, response...at the root route
     res.send(`
         <div>
             Your ID is: ${req.session.userId}
@@ -25,9 +25,8 @@ app.get('/', (req, res) => { // request, response...at the root route
     `);
 });
 
-
-app.post('/', async (req, res) => { // we could add our bodyParser Middleware here or up above in 'app.use'!
-    const { email, password, passwordConfirmation } = req.body;
+app.post('/signup', async (req, res) => { // we could add our bodyParser Middleware here or up above in 'app.use'!
+    const { email, password, passwordConfirmation } = req.body; // all form data is contained inside req.body
 
     const existingUser = await usersRepo.getOneBy({ email }); // key and variable value are the same so can write short-hand
     if (existingUser) {
@@ -44,6 +43,42 @@ app.post('/', async (req, res) => { // we could add our bodyParser Middleware he
     // Store the id of that user inside the users cookie
     req.session.userId = user.id; // req.session is an object added by cookie-session library!
     res.send('Account created');
+});
+
+
+app.get('/signout', (req, res) => {
+    req.session = null; // clear out any cookie data the user has
+    res.send('You are logged out');
+});
+
+
+app.get('/signin', (req, res) => {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <button>Sign In</button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await usersRepo.getOneBy({ email });
+
+    if (!user) { // user object is undefined, i.e, user not found
+        return res.send('Email not found');
+    }
+
+    if (user.password !== password) {
+        return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+    res.send('You are signed in!');
 });
 
 
