27f7003c5d316dbfcc5526e95f0841f18d11c7d2
Christopher Chua <chuachristopher@hotmail.com>
2020-05-31 20:11:12 +0800

    Ecommerce added sanitisation and validators

--- code/PART 2/ecommerce/routes/admin/auth.js
+++ code/PART 2/ecommerce/routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users'); // import in from our UsersRepo we created
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -9,24 +11,42 @@ router.get('/signup', (req, res) => { // request, response...at the root route
     res.send(signupTemplate({ req })); // signup template needs req object
 });
 
-router.post('/signup', async (req, res) => { // we could add our bodyParser Middleware here or up above in 'router.use'!
-    const { email, password, passwordConfirmation } = req.body; // all form data is contained inside req.body
+router.post('/signup', 
+    [
+        check('email')
+            .trim()
+            .normalizeEmail()
+            .isEmail()
+            .withMessage('Must be a valid email'),
+        check('password')
+            .trim()
+            .isLength({ min: 4, max: 20})
+            .withMessage('Must be between 4 and 20 characters'),
+        check('passwordConfirmation')
+            .trim()
+            .isLength({ min: 4, max: 20})
+            .withMessage('Must be between 4 and 20 characters')
+    ], 
+    async (req, res) => { // we could add our bodyParser Middleware here or up above in 'router.use'!
+        const errors = validationResult(req); // errors is array of objects
+        console.log(errors);
+        const { email, password, passwordConfirmation } = req.body; // all form data is contained inside req.body
 
-    const existingUser = await usersRepo.getOneBy({ email }); // key and variable value are the same so can write short-hand
-    if (existingUser) {
-        return res.send('Email already in use');
-    }
+        const existingUser = await usersRepo.getOneBy({ email }); // key and variable value are the same so can write short-hand
+        if (existingUser) {
+            return res.send('Email already in use');
+        }
 
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords must match');
-    }
-    console.log(req.body);
+        if (password !== passwordConfirmation) {
+            return res.send('Passwords must match');
+        }
+        console.log(req.body);
 
-    // Create a user in our user repo to represent this person
-    const user = await usersRepo.create({ email, password });
-    // Store the id of that user inside the users cookie
-    req.session.userId = user.id; // req.session is an object added by cookie-session library!
-    res.send('Account created');
+        // Create a user in our user repo to represent this person
+        const user = await usersRepo.create({ email, password });
+        // Store the id of that user inside the users cookie
+        req.session.userId = user.id; // req.session is an object added by cookie-session library!
+        res.send('Account created');
 });
 
 
