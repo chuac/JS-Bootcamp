279c2b5afc4f051f2d9ffeae00deba8e77304c29
Christopher Chua <chuachristopher@hotmail.com>
2020-06-03 14:29:30 +0800

    Ecommerce creating a cart for the user

--- code/PART 2/ecommerce/routes/carts.js
+++ code/PART 2/ecommerce/routes/carts.js
@@ -1,13 +1,26 @@
 const express = require('express');
 
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
 
 const router = express.Router();
 
 
 // receive a POST request to add an item to a cart
-router.post('/cart/products', (req, res) => {
-    console.log(req.body.productId);
+router.post('/cart/products', async (req, res) => {
+    //console.log(req.body.productId);
+
+    let cart;
+    if (!req.session.cartId) { // no cart found in the user's cookies
+        cart = await cartsRepo.create({ items: [] }); // create a new cart for the user
+        req.session.cartId = cart.id; // add the cart ID into user's cookies
+    } else {
+        cart = await cartsRepo.getOne(req.session.cartId); // otherwise, get their cart from the db
+    }
+    console.log(cart);
+
+    // either increment quantity for existing product
+    // or add new product into the cart
 
     res.send('Product added to cart');
 })
