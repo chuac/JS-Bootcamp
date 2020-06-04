9394a75b4b10115c98d823d1e5be5a2aaaf62345
Christopher Chua <chuachristopher@hotmail.com>
2020-06-04 10:34:52 +0800

    Ecommerce cart styling and totalling cart price

--- code/PART 2/ecommerce/views/carts/show.js
+++ code/PART 2/ecommerce/views/carts/show.js
@@ -1,17 +1,64 @@
 const layout = require('../layout');
 
 module.exports = ({ items }) => {
+    //// Calculating the total price of the cart in two ways: for-loop vs reduce statement
+    // let totalPrice = 0;
+    // for (let item of items) {
+    //     totalPrice += item.product.price * item.quantity;
+    // }
+
+    const totalPrice = items.reduce((accumulator, item) => {
+        return accumulator + (item.quantity * item.product.price);
+    }, 0);
+
+
     const renderedItems = items.map((item) => {
         return `
-            <div>${item.product.title} - $${item.product.price} * ${item.quantity}</div>
+            <div class="cart-item message">
+                <h3 class="subtitle">${item.product.title}</h3>
+                <div class="cart-right">
+                    <div>
+                        $${item.product.price}  X  ${item.quantity} = 
+                    </div>
+                    <div class="price is-size-4">
+                        $${item.product.price * item.quantity}
+                    </div>
+                    <div class="remove">
+                        <form method="POST">
+                            <button class="button is-danger">                  
+                                <span class="icon is-small">
+                                    <i class="fas fa-times"></i>
+                                </span>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         `;
     }).join('');
 
 
     return layout({
         content: `
-            <h1>Cart</h1>
-            ${renderedItems}
+            <div id="cart" class="container">
+                <div class="columns">
+                    <div class="column"></div>
+                    <div class="column is-four-fifths">
+                        <h3 class="subtitle"><b>Shopping Cart</b></h3>
+                        <div>
+                            ${renderedItems}
+                        </div>
+                        <div class="total message is-info">
+                            <div class="message-header">
+                                Total
+                            </div>
+                            <h1 class="title">$${totalPrice}</h1>
+                            <button class="button is-primary">Buy</button>
+                        </div>
+                    </div>
+                    <div class="column"></div>
+                </div>
+            </div>
         `
     });
 }
