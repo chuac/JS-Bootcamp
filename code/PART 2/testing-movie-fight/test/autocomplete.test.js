8e38e20bd0b2e4329863d0da39c1f39f977828de
Christopher Chua <chuachristopher@hotmail.com>
2020-06-04 14:15:09 +0800

    Testing faking a DOM event

--- code/PART 2/testing-movie-fight/test/autocomplete.test.js
+++ code/PART 2/testing-movie-fight/test/autocomplete.test.js
@@ -1,6 +1,8 @@
 // no need to require our actual .js file. Mocha does that for us
 
-it('Shows an autocomplete widget', () => {
+beforeEach(() => { // runs before every test
+    document.querySelector('#target').innerHTML = '';
+
     createAutoComplete({
         root: document.querySelector('#target'),
         fetchData() { // just some fake data
@@ -14,10 +16,24 @@ it('Shows an autocomplete widget', () => {
             return movie.Title;
         }
     });
+    // makes sure we rebuild a clean autocomplete widget before every test
+})
 
+
+it('Dropdown starts closed', () => {
     const dropdown = document.querySelector('.dropdown');
     // make sure dropdown element starts off being closed. no "is-active" class
     expect(dropdown.className).not.to.include('is-active'); // check Chai Assertion Library documentation
 });
 
 
+it('After searching, dropdown opens up', () => {
+    const input = document.querySelector('.input');
+
+    input.value = 'avengers';
+    input.dispatchEvent(new Event('input')); // fake a DOM event of 'input'
+
+    const dropdown = document.querySelector('.dropdown');
+
+    expect(dropdown.className).to.include('is-active');
+});
