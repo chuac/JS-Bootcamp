1becf70ed5a2ced39f678ba301aa96e76c4c8af3
Christopher Chua <chuachristopher@hotmail.com>
2020-06-04 13:07:53 +0800

    Testing lodash using Assert module

--- code/PART 2/testing-hidash/index.test.js
+++ code/PART 2/testing-hidash/index.test.js
@@ -1,6 +1,10 @@
 // testing index.js
 
+// https://nodejs.org/api/assert.html
+const assert = require('assert');
+
 const { forEach, map } = require('./index');
+
 
 const test = (desc, fn) => {
     console.log('----', desc);
@@ -18,26 +22,20 @@ test('The forEach function', () => {
         sum += value;
     });
 
-    if (sum !== 7) {
-        throw new Error('Expected summing array to equal 6');
-    }
+    assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
 });
-
 
 test('The map function', () => {
     const result = map([1, 2, 3], (value) => {
         return value * 2;
     });
     // result === [2, 4, 6]
-    if (result[0] !== 2) {
-        throw new Error(`Expected to find 2, but found ${result[0]}`);
-    }
-    if (result[1] !== 4) {
-        throw new Error(`Expected to find 4, but found ${result[1]}`);
-    }
-    if (result[2] !== 6) {
-        throw new Error(`Expected to find 6, but found ${result[2]}`);
-    }
+
+    // assert.strictEqual(result[0], 2); // optional extra error message
+    // assert.strictEqual(result[1], 4);
+    // assert.strictEqual(result[2], 6);
+
+    assert.deepStrictEqual(result, [2, 4, 6]); // can also use deepStrictEqual instead of strictEqual
 })
 
 
