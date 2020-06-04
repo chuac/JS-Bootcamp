0a937491578c9610575fc575d106db93bee786f9
Christopher Chua <chuachristopher@hotmail.com>
2020-06-04 21:26:07 +0800

    Test-me Our Own Testing Framework. Project setup

---
+++ code/PART 2/test-me/index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+// Creating our own testing framework that can be run in any directory with CLI-arg of "tme"
+console.log('Running test!!');
---
+++ code/PART 2/test-me/runner.js
@@ -0,0 +1,13 @@
+// most of code for File Collection, calling a function to setup the environment, then execute each file that was found
+
+class Runner {
+    constructor() {
+        this.files = []; // contains all the files we found
+    }
+
+    collectFiles() {
+        
+    }
+};
+
+module.exports = Runner;
