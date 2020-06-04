cc2ee885a43e061814ee7e296becef772048b686
Christopher Chua <chuachristopher@hotmail.com>
2020-06-04 13:46:43 +0800

    Testing movie fight showing autocomplete widget

--- code/PART 2/testing-movie-fight/test/autocomplete.test.js
+++ code/PART 2/testing-movie-fight/test/autocomplete.test.js
@@ -1,5 +1,17 @@
 // no need to require our actual .js file. Mocha does that for us
 
-it('works!!', () => {
-
+it('Shows an autocomplete widget', () => {
+    createAutoComplete({
+        root: document.querySelector('#target'),
+        fetchData() { // just some fake data
+            return [
+                { Title: 'Avengers' },
+                { Title: 'Not Avengers' },
+                { Title: 'Some other movie' }
+            ]
+        },
+        renderOption(movie) {
+            return movie.Title;
+        }
+    })
 });
