ee1d2960a383d583dc6fe4ed1a62db177342b11c
Christopher Chua <chuachristopher@hotmail.com>
2020-06-04 12:36:23 +0800

    Testing hidash basic forEach

---
+++ code/PART 2/testing-hidash/index.js
@@ -0,0 +1,13 @@
+// small node code to practice testing on
+module.exports = {
+    forEach(arr, func) { // implement our own forEach
+        // for(let i = 0; i < arr.length; i++) {
+        //     const value = arr[i];
+        //     func(value, i);
+        // }
+        for (let index in arr) { // for-in loop loops over the indexes in the array, for-of loops over the items
+            //const value = arr[index];
+            func(arr[index], index);
+        }
+    }
+}
