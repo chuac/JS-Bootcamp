7cac5ea1df2b4e138a4e862e582fe088a67aee4e
Christopher Chua <chuachristopher@hotmail.com>
2020-06-05 10:41:04 +0800

    Test-me starting Breadth First Search

--- code/PART 2/test-me/index.js
+++ code/PART 2/test-me/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 // Creating our own testing framework that can be run in any directory with CLI-arg of "tme"
-console.log('Running test!!');
+
+const Runner = require('./runner');
+const runner = new Runner();
+
+
+const run = async () => { // need this helper function to call await functions
+    const results = await runner.collectFiles(process.cwd()); // process.cwd() to get the path whereever this app is run (by calling tme)
+    console.log(results);
+};
+run();
--- code/PART 2/test-me/runner.js
+++ code/PART 2/test-me/runner.js
@@ -1,12 +1,18 @@
 // most of code for File Collection, calling a function to setup the environment, then execute each file that was found
 
+const fs = require('fs');
+
+
 class Runner {
     constructor() {
         this.files = []; // contains all the files we found
     }
 
-    collectFiles() {
-        
+    async collectFiles(targetPath) { // breadth-first search
+        // targetPath === /Users/chris/Documents/projects/movie-fight , for example
+        const files = await fs.promises.readdir(targetPath);
+
+        return files;
     }
 };
 
