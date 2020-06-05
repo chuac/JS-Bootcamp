9d5e4da7e7662681b19acc59afec11cce6e33d43
Christopher Chua <chuachristopher@hotmail.com>
2020-06-05 13:25:41 +0800

    Test-me complete with extra test for samplewebapp

--- code/PART 2/test-me/samplewebproject/test/app.test.js
+++ code/PART 2/test-me/samplewebproject/test/app.test.js
@@ -8,11 +8,12 @@ it('has a text input', async () => {
     assert(input); // would throw an error if 'input' evaluates to a falsy value (like null)
 });
 
-it('shows a success message with a valid email', async () => {
+
+it('shows a success message with a VALID email', async () => {
     const dom = await render('index.html');
 
     const input = dom.window.document.querySelector('input');
-    input.value = 'zzzz@zzzzz.com';
+    input.value = 'zzzz@zzzzz.com'; // the app just looks for one '@' symbol
 
     dom.window.document
         .querySelector('form')
@@ -21,5 +22,20 @@ it('shows a success message with a valid email', async () => {
     const h1 = dom.window.document.querySelector('h1');
 
     assert.strictEqual(h1.innerHTML, 'Looks good!');
+});
 
+
+it('shows a fail message with an INVALID email', async () => {
+    const dom = await render('index.html');
+
+    const input = dom.window.document.querySelector('input');
+    input.value = 'zzzz_zzzzz.com'; // no '@' symbol will be found
+
+    dom.window.document
+        .querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit')); // fake a submit event
+    
+    const h1 = dom.window.document.querySelector('h1');
+
+    assert.strictEqual(h1.innerHTML, 'Invalid email');
 });
